abc28eb2c352de04d6b1b1cf2c113a5534774b55
Eliot Sykes <eliotsykes@gmail.com>
2014-12-11 21:30:38 +0000

    require shoulda matchers in rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'shoulda/matchers'
 require 'capybara/rails'
 require 'capybara/rspec'
 require 'email_spec'
