a4e402102095b007823e8dc3d23175df1f800952
Eliot Sykes <eliotsykes@gmail.com>
2014-12-11 16:24:55 +0000

    Added requires for capybara+rails+rspec setup to rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,8 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'capybara/rails'
+require 'capybara/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
