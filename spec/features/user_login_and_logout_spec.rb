b9d40a431287e976914928601624a00b637de1aa
Eliot Sykes <eliotsykes@gmail.com>
2015-01-03 12:08:36 +0000

    Work In Progress: user login and logout spec

---
+++ spec/features/user_login_and_logout_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+feature "User logs in and logs out", :type => :feature do
+  
+  scenario do
+    
+    user = create(:user, email: "someone@example.tld", password: "somepassword")
+
+    visit "/"
+
+    click_link "Log in"
+    expect(current_path).to eq(new_user_session_path)
+
+    fill_in "Email", with: "someone@example.tld"
+    fill_in "Password", with: "somepassword"
+    click_button "Log in"
+
+    expect(current_path).to eq "/"
+    expect(page).to have_content "Signed in successfully."
+    expect(page).to have_content "Hello, someone@example.tld"
+
+    click_button "Log out"
+
+    expect(current_path).to eq "/"
+    expect(page).to have_content "Logged out successfully."
+    expect(page).not_to have_content "someone@example.tld"    
+
+  end
+
+
+
+end
