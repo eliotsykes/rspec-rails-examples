b077dc73f5635fad83b2d27f0c9b8c16a3607380
Eliot Sykes <eliotsykes@gmail.com>
2015-03-28 20:53:24 +0000

    More accurate database cleaner strategy selection for feature specs

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -49,9 +49,17 @@ RSpec.configure do |config|
     DatabaseCleaner.strategy = :transaction
   end
 
-  # Use truncation (not transactions) with JavaScript-dependent specs:
-  config.before(:each, js: true) do
-    DatabaseCleaner.strategy = :truncation
+  config.before(:each, type: :feature) do
+    if Capybara.current_driver == :rack_test
+      # :rack_test driver's Rack app under test shares database connection
+      # with the specs, so we can use transaction strategy for speed.
+      DatabaseCleaner.strategy = :transaction
+    else
+      # Non-:rack_test driver is probably a driver for a JavaScript browser
+      # with a Rack app under test that does *not* share a database 
+      # connection with the specs, so we must use truncation strategy.
+      DatabaseCleaner.strategy = :truncation
+    end
   end
 
   config.before(:each) do
