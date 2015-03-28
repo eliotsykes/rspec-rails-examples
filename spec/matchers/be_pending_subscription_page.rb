5a921a0f1db9f3d804ab0bb3717bdad6992cb742
Eliot Sykes <eliotsykes@gmail.com>
2015-03-28 17:05:13 +0000

    Custom RSpec Matchers: improve config and docs

---
+++ spec/matchers/be_pending_subscription_page.rb
@@ -0,0 +1,50 @@
+# It's important that the matcher is in a module and the module
+# has an instance method that will be available to the specs.
+# The module name here will need to match the module name
+# in the configuration specified in spec/support/matchers.rb
+module Matchers
+
+  # This instance method is what will be called in the specs
+  # e.g. expect(page).to be_pending_subscription_page
+  # 
+  # This method is made available thanks to the
+  # 'config.include Matchers ...' line in 
+  #  spec/support/matchers.rb
+  def be_pending_subscription_page(*args)
+    BePendingSubscriptionPage.new(*args)
+  end
+  
+  class BePendingSubscriptionPage
+    include ::Rails.application.routes.url_helpers # Include _path & _url helpers
+
+    # failure_message is a required method for RSpec matchers
+    attr_accessor :failure_message
+
+    # matches? is a required method for RSpec matchers
+    def matches?(actual_page)
+      expected_title = "Check your inbox"
+      if actual_page.has_no_title?(expected_title)
+        self.failure_message = "\nexpected title: #{expected_title}\n     got title: #{actual_page.title}\n"
+        return false
+      end
+
+      # pending_subscriptions_path is available here thanks to
+      # including ::Rails.application.routes.url_helpers in this
+      # class.
+      expected_path = pending_subscriptions_path
+      if actual_page.current_path != expected_path
+        self.failure_message = "\nexpected current path: #{expected_path}\n     got current path: #{actual_page.current_path}\n"
+        return false
+      end
+
+      expected_content = "Please check your inbox and click the confirmation link to complete your subscription."
+      if actual_page.has_no_content?(expected_content)
+        self.failure_message = "\nexpected content: #{expected_content}\n     got content: #{actual_page.text}\n"
+        return false
+      end
+
+      true
+    end
+  end
+
+end
---
+++ spec/support/matchers.rb
@@ -0,0 +1,19 @@
+# Setting up Custom Matchers
+
+# 1. Create a file like this one you're reading in spec/support/matchers.rb:
+
+Dir[Rails.root.join("spec/matchers/**/*.rb")].each { |f| require f }
+
+RSpec.configure do |config|
+  config.include Matchers
+end
+
+# 2. Write custom matcher classes in spec/matchers/ directory
+# 
+#    Each matcher class will need to be in a module called 
+#    "Matchers" to work with the RSpec configuration specified
+#    above, see the spec/matchers directory for an example.
+
+# Suggested docs
+# --------------
+# "Custom Matchers" section at http://www.rubydoc.info/gems/rspec-expectations/RSpec/Matchers
