6fae8fac708a0a641b5d1239f47bd7a26f880498
Eliot Sykes <eliotsykes@gmail.com>
2015-05-26 15:28:34 +0100

    Fixed stray quote in devise config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'e@rspec-rails-examples.tld"'
+  config.mailer_sender = 'e@rspec-rails-examples.tld'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
