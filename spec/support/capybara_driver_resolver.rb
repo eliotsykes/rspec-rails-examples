f205fb67f46f70cee0e611fd76fcff4e73c9f2f4
Eliot Sykes <eliotsykes@gmail.com>
2015-09-03 07:17:03 +0100

    Clarify browsers driven by Capybara drivers

--- spec/support/capybara_driver_resolver.rb
+++ spec/support/capybara_driver_resolver.rb
@@ -4,13 +4,12 @@
 # with descriptive names related to the underlying browser's HTML5 support.
 module CapybaraDriverResolver
 
-  # At time of writing: These drivers wrap phantomjs or chrome and they both support
-  # native date inputs.
-  WITH_NATIVE_DATE_INPUT = [:poltergeist_billy, :poltergeist, :selenium_chrome_billy]
+  FIREFOX_DRIVERS   = [:selenium_billy, :selenium]
+  CHROME_DRIVERS    = [:selenium_chrome_billy]
+  PHANTOMJS_DRIVERS = [:poltergeist_billy, :poltergeist]
 
-  # At time of writing: These drivers wrap Firefox browsers that don't support native
-  # date inputs.
-  WITHOUT_NATIVE_DATE_INPUT = [:selenium_billy, :selenium]
+  WITH_NATIVE_DATE_INPUT    = PHANTOMJS_DRIVERS + CHROME_DRIVERS
+  WITHOUT_NATIVE_DATE_INPUT = FIREFOX_DRIVERS
 
   def driver_with(options)
 
