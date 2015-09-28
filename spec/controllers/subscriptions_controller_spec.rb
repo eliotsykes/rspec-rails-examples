303963d5b5f9bab45351be4ed2cac580721eb724
Vinh Nguyen <vinh.nglx@gmail.com>
2015-09-28 14:05:20 +0700

    Add one test case for subscription controller

--- spec/controllers/subscriptions_controller_spec.rb
+++ spec/controllers/subscriptions_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe SubscriptionsController, :type => :controller do
 
   context "GET new" do
-    
+
     it "assigns a blank subscription to the view" do
       get :new
       expect(assigns(:subscription)).to be_a_new(Subscription)
@@ -12,7 +12,7 @@ RSpec.describe SubscriptionsController, :type => :controller do
   end
 
   context "POST create" do
-    
+
     it "redirects to pending subscriptions page" do
       params = { subscription: { email: "e@example.tld", start_on: "2014-12-31" } }
       post :create, params
@@ -23,20 +23,28 @@ RSpec.describe SubscriptionsController, :type => :controller do
     it "calls Subscription.create_and_request_confirmation(params)" do
       email = "e@example.tld"
       start_on = "2015-02-28"
-      
+
       expect(Subscription).to receive(:create_and_request_confirmation)
         .with({ email: email, start_on: start_on })
 
       post :create, { subscription: { email: email, start_on: start_on } }
     end
 
+    it "raises an error if missing params email" do
+      params = { subscription: { start_on: "2015-09-28" } }
+
+      expect do
+        post :create, params
+      end.to raise_error ActiveRecord::RecordInvalid
+    end
+
   end
 
   context "GET confirm" do
-    
+
     it "confirms the subscription" do
-      subscription = Subscription.create!(
-        email: "e@e.tld", 
+      subscription = create(:subscription,
+        email: "e@e.tld",
         confirmation_token: Subscription.generate_confirmation_token
       )
       expect(subscription.confirmed?).to eq(false)
