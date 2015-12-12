43db5a4a1a560ca9b10dc885adfff2caf0d4dff3
Eliot Sykes <eliotsykes@gmail.com>
2015-12-12 15:20:59 +0000

    Capybara.using_wait_time whilst recording Stripe interactions

--- spec/features/user_upgrades_spec.rb
+++ spec/features/user_upgrades_spec.rb
@@ -9,16 +9,19 @@ feature "User upgrades", js: true do
     login user
     click_button "Upgrade Membership"
 
-    within_frame "stripe_checkout_app" do
-      fill_in "Email", with: user.email
-      fill_in_stripe_field "#card_number", with: "4242424242424242"
-      fill_in_stripe_field "#cc-exp", with: "12/22"
-      fill_in "CVC", with: '123'
-      click_button "Pay $9.99"
+    Capybara.using_wait_time 5.seconds do
+      within_frame "stripe_checkout_app" do
+        fill_in "Email", with: user.email
+        fill_in_stripe_field "#card_number", with: "4242 4242 4242 4242"
+        fill_in_stripe_field "#cc-exp", with: "12/22"
+        fill_in "CVC", with: '123'
+        click_button "Pay $9.99"
+      end
+      expect(page).to have_no_button "Upgrade Membership"
     end
 
+    expect(page).to have_content "Charged!"
     expect(page).to have_content "Thank you for upgrading your membership!"
-    expect(page).not_to have_css "button", text: "Upgrade Membership"
     expect(user.reload.premium?).to eq true
   end
 
