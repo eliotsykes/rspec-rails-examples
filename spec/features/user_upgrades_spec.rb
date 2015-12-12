7f5480291583f93df68dcb3e3fa8dcdad0b1ba69
Eliot Sykes <eliotsykes@gmail.com>
2015-12-12 15:46:09 +0000

    Use a Puffing Billy cache scope

--- spec/features/user_upgrades_spec.rb
+++ spec/features/user_upgrades_spec.rb
@@ -5,19 +5,21 @@ feature "User upgrades", js: true do
   scenario "successfully" do
     user = create(:user, role: "standard")
 
-    visit new_user_session_path
-    login user
-    click_button "Upgrade Membership"
+    use_billy_cache "stripe checkout" do
+      visit new_user_session_path
+      login user
+      click_button "Upgrade Membership"
 
-    Capybara.using_wait_time 5.seconds do
-      within_frame "stripe_checkout_app" do
-        fill_in "Email", with: user.email
-        fill_in_stripe_field "#card_number", with: "4242 4242 4242 4242"
-        fill_in_stripe_field "#cc-exp", with: "12/22"
-        fill_in "CVC", with: '123'
-        click_button "Pay $9.99"
+      Capybara.using_wait_time 5.seconds do
+        within_frame "stripe_checkout_app" do
+          fill_in "Email", with: user.email
+          fill_in_stripe_field "#card_number", with: "4242 4242 4242 4242"
+          fill_in_stripe_field "#cc-exp", with: "12/22"
+          fill_in "CVC", with: '123'
+          click_button "Pay $9.99"
+        end
+        expect(page).to have_no_button "Upgrade Membership"
       end
-      expect(page).to have_no_button "Upgrade Membership"
     end
 
     expect(page).to have_content "Charged!"
